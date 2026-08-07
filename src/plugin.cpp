31ced8ede92534fb2ca06f3f0ab8a6466ca6844f
Michael Amiethyst <michaelamiethyst@gmail.com>
2026-08-06 17:55:12 -0700

    0.1.0 - Added Console logging back

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -5,6 +5,7 @@ SKSEPluginLoad(const SKSE::LoadInterface *skse) {
     // be printed to
     SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message *message) {
         if (message->type == SKSE::MessagingInterface::kDataLoaded) {
+            RE::ConsoleLog::GetSingleton()->Print("SKSE Hello World loaded!");
             // kDataLoaded is delivered while the main-menu UI is being set up.
             // Queue the notification so it runs on the UI thread after setup.
             SKSE::GetTaskInterface()->AddUITask([] {
